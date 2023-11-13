d763a99f58b1fe351585c3a11a6a42a9812d8fa9
Tom Leonard Abella <tomleonardabella@gmail.com>
2023-11-13 15:13:32 +0800

    up

--- src/App.jsx
+++ src/App.jsx
@@ -31,15 +31,26 @@ function App() {
       window.ReactNativeWebView.postMessage(JSON.stringify(errObj));
   }
 
+  const [receivedData, setReceivedData] = useState(null);
+
   useEffect(() => {
     // Add an event listener to handle messages sent from React Native
     const handleMessage = (event) => {
-      
-      const receivedData = JSON.parse(event.data);
-      alert('Received data in ReactJS:', receivedData);
+      // Handle messages sent from React Native
+      const data = event.data;
+
+      try {
+        const parsedData = JSON.parse(data);
+        // Update state with the received data
+        setReceivedData(parsedData);
+      } catch (error) {
+        console.error('Error parsing received data:', error);
+      }
     };
 
     window.addEventListener('message', handleMessage);
+
+    // Clean up the event listener when the component is unmounted
     return () => {
       window.removeEventListener('message', handleMessage);
     };
@@ -47,11 +58,17 @@ function App() {
 
   return (
     <div className="container">
+      {receivedData && (
+        <div>
+          <p>Received data in ReactJS:</p>
+          <pre>{JSON.stringify(receivedData, null, 2)}</pre>
+        </div>
+      )}
       <PayPalButton
         createOrder={(data, actions) => _createOrder(data, actions)}
         onApprove={(data, actions) => _onApprove(data, actions)}
         onCancel={() => _onError("CANCELED")}
-        onError={(err) => _onError("ERROE")}
+        onError={(err) => _onError("ERROR")}
       />
     </div>
   );
